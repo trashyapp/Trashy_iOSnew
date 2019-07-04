f84e9bd9aab80a5c86abf42cde0248d6b20a19b1
Kai Zheng <mofawaw@Kais-MBP-2.fritz.box>
2019-07-04 20:25:51 +0200

    settings further improvements

---
+++ Trashy/ViewControllers/Einstellungen/SettingsSelectionTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsSelectionTVCell.swift
+//  Trashy
+//
+//  Created by Kai Zheng on 04.07.19.
+//  Copyright © 2019 Trashy. All rights reserved.
+//
+
+import UIKit
+
+class SettingsSelectionTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Trashy/ViewControllers/Einstellungen/SettingsTVC.swift
@@ -0,0 +1,52 @@
+//
+//  SettingsTVC.swift
+//  
+//
+//  Created by Kai Zheng on 04.07.19.
+//
+
+import UIKit
+
+class SettingsTVC: UITableViewController {
+
+    @IBOutlet var settingsShadowViews: [RoundView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        for i in 0..<settingsShadowViews.count {
+            setUpShatten(view: settingsShadowViews[i], op: 0.2, radius: 8.0)
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func setUpShatten(view: UIView, op: Float, radius: CGFloat) {
+        view.layer.shadowColor = UIColor.darkGray.cgColor
+        view.layer.shadowOpacity = op
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = radius
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            print("Notification")
+        case 1:
+            print("Place")
+            performSegue(withIdentifier: "toSettingsSelectionTVC", sender: self)
+        case 2:
+            print("Language")
+            performSegue(withIdentifier: "toSettingsSelectionTVC", sender: self)
+        default: break
+        }
+    }
+}
